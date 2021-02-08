b30441a2147eceae519d6dd0c38242ff2fa0d3f0
Yona Greenwood <yona.greenwood@gmail.com>
2021-02-08 21:36:08 +0200

    Milestone 1

--- js/index.js
+++ js/index.js
@@ -1,19 +1,17 @@
 const button = document.querySelector(".button");
 const number = document.querySelector(".select-number");
 
-
-function fibonacci(){
+function fibonacci() {
   let x = 1;
   let y = 0;
   let result = 0;
-  for (let i=0; i<number.value; i++){
+  for (let i = 0; i < number.value; i++) {
     result = x + y;
     x = y;
     y = result;
   }
-  document.querySelector(".output").innerHTML= result;
-  console.log(result)
+  document.querySelector(".output").innerHTML = result;
+  console.log(result);
 }
 
-button.addEventListener('click', fibonacci);
-
+button.addEventListener("click", fibonacci);
