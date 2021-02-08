120e507296eb567810ff1736c7e839a07024303f
Yona Greenwood <yona.greenwood@gmail.com>
2021-02-08 21:33:02 +0200

    Hw

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const button = document.querySelector(".button");
+const number = document.querySelector(".select-number");
+
+
+function fibonacci(){
+  let x = 1;
+  let y = 0;
+  let result = 0;
+  for (let i=0; i<number.value; i++){
+    result = x + y;
+    x = y;
+    y = result;
+  }
+  document.querySelector(".output").innerHTML= result;
+  console.log(result)
+}
+
+button.addEventListener('click', fibonacci);
+
